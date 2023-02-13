1e3cdfb72c6773fd706132aae94ca6ebb73f53a8
jamlee <1575103047@qq.com>
2023-02-13 20:37:30 +0800

    feat(::memo::):doc content

--- start_node_go_bash.sh
+++ start_node_go_bash.sh
@@ -1,5 +1,8 @@
-conda activate js
+#!/bin/bash
+
+conda activate snippet 
 source ~/.bashrc
+
 notedir=`pwd`
 export PATH=/root/go/bin:$PATH 
 jupyter-lab --allow-root --NotebookApp.token='' --NotebookApp.password='' --NotebookApp.ip='0.0.0.0' --NotebookApp.notebook_dir="${notedir}"
